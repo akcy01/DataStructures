1e9d9a364987ba5b30550e0e922dc568c9ba35ba
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-15 15:36:20 +0300

    SortedList Completed.

---
+++ DataStructures/SortedLists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.SortedLists
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Tanımlama ve Eleman Ekleme */
+            var list = new SortedList()
+            {
+                {1 ,"Oğuz"},
+                {2 ,"Kemal"},
+                {5,"Beyza" },
+                {3, "Lale" },
+                {9,"Ayşe" }
+            }; //Sıralı halde vermesek de otomatikmen key'e göre sıralayacaktır makine.
+        }
+    }
+}
