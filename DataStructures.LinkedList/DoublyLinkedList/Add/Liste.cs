53b768b5a1ffde428e4893e1a02817d836a8fdde
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-24 18:01:35 +0300

    DoublyLinkedList started.

---
+++ DataStructures.LinkedList/DoublyLinkedList/Add/Liste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.DoublyLinkedList.Add
+{
+    public class Liste
+    {
+        Node head;
+        Node tail;
+
+        public Liste()
+        {
+            head = null; 
+            tail = null;
+        }
+    }
+}
---
+++ DataStructures.LinkedList/DoublyLinkedList/Readme.md
@@ -0,0 +1,4 @@
+﻿#Doubly LinkedList
+Singly yani tek yönlü doğrusal listelere göre farklılıkları vardır.Head'ın hem sağında hem de solunda bir değer vardır artık.Yani head'ın next'i varken artık prev'i de olacaktır.
+Artık her düğümün bir next'i ve prev'i olacaktır.
+Son düğümün next'i null'u gösteriyordu.Bunda da son düğümün next'i null'u göstermelidir!!
