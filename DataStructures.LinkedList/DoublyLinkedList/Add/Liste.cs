e63ee39cc1b126e22c25a83f5baef2023c7679b1
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-31 19:14:13 +0300

    AddMidle method completed.

--- DataStructures.LinkedList/DoublyLinkedList/Add/Liste.cs
+++ DataStructures.LinkedList/DoublyLinkedList/Add/Liste.cs
@@ -51,26 +51,54 @@ namespace DataStructures.LinkedList.DoublyLinkedList.Add
         }
         public void AddMiddle(int data,int index) /* Araya ekleme */
         {
+            bool sonuc = false;
             Node eleman = new Node(data);
 
             if (head == null && index == 0)
             {
+                sonuc = true;
                 head = tail = eleman;
                 Console.WriteLine("Liste oluşturuldu.İlk eleman eklendi");
             }
             else if(head !=null && index == 0)
             {
-               Push(data);
+                sonuc = true;
+                Push(data);
             }
             else
             {
+                int i = 0;
                 Node temp = head;
+                Node temp2 = temp;
                 while(temp.next != null)
                 {
+                    if(i == index)
+                    {
+                        sonuc = true;
+                        temp2.next = eleman;
+                        eleman.prev = temp2;
 
+                        eleman.next = temp;
+                        temp.prev = eleman;
+                        Console.WriteLine("Araya ekleme yapıldı");
+                        i++;
+                        break;
+                    }
                 }
+                if (i == index) /* Kullanıcı son düğüme ekleme yapmak istediğinde.While'dan çıktığında son düğümde olacak.*/
+                {
+                    sonuc = true;
+                    temp2.next = eleman;
+                    eleman.prev = temp2;
 
-                Console.WriteLine("Araya eleman eklendi.");
+                    eleman.next = tail;
+                    tail.prev = eleman;
+                    Console.WriteLine("Araya eleman eklendi.");
+                }
+            }
+            if(sonuc == false)
+            {
+                Console.WriteLine("Hatalı index seçimi yaptınız.");
             }
         }
     }
