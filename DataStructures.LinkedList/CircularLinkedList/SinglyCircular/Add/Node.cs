529de8c88ebe26450f06c57fe2d28b9562fccbcd
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-01-05 16:41:40 +0300

    SinglyCircularList completed.

---
+++ DataStructures.LinkedList/CircularLinkedList/SinglyCircular/Add/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+using DataStructures.LinkedList;
+using DataStructures.LinkedList.CircularLinkedList;
+using DataStructures.LinkedList.CircularLinkedList.SinglyCircular;
+using DataStructures.LinkedList.CircularLinkedList.SinglyCircular.Add;
+
+namespace DataStructures.LinkedList.CircularLinkedList.SinglyCircular.Add
+{
+    internal class Node
+    {
+    }
+}
---
+++ DataStructures.LinkedList/CircularLinkedList/SinglyCircular/Readme.md
@@ -0,0 +1,2 @@
+﻿#SinglyCircularList Nedir ? 
+En büyük farkı son düğümün yani tail'in next'inin head'i göstermesi.!
