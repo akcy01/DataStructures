067bf1f376a9d6e400227a068ba85b63c42cf0f5
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-01-15 13:51:47 +0300

    DeleteMiddle completed.

--- DataStructures.LinkedList/CircularLinkedList/DoublyCircular/Deletion/List.cs
+++ DataStructures.LinkedList/CircularLinkedList/DoublyCircular/Deletion/List.cs
@@ -100,5 +100,45 @@ namespace DataStructures.LinkedList.CircularLinkedList.DoublyCircular.Deletion
                 Console.WriteLine("Sondan eleman silindi");
             }
         }
+        public void DeleteMiddle(int index)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Liste boş");
+            }
+            else if (head.next == head && index == 0)
+            {
+                head = tail = null;
+                Console.WriteLine("Eleman silindi listede eleman kalmadı");
+            }
+            else if (head.next != head && index == 0)
+            {
+                DeleteBeginning();
+            }
+            else
+            {
+                Node temp = head;
+                Node temp2 = temp;
+                int i = 0;
+                while(temp != tail)
+                {
+                    if(i == index)
+                    {
+                        temp2.next = temp.next;
+                        temp.next.prev = temp2;
+                        Console.WriteLine("Aradan eleman silindi");
+                        i++;
+                        break;
+                    }
+                    temp2 = temp;
+                    temp = temp.next;
+                    i++;
+                }
+                if(i == index)
+                {
+                    DeleteEnd();
+                }
+            }
+        }
     }
 }
