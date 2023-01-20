6b0c97d4ff55685548cc2f2311f413761a02733b
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-01-20 22:27:18 +0300

    Push method completed.

---
+++ DataStructures.Stack/StackStructure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Stack
+{
+    public class StackStructure
+    {
+        Node top;
+        public StackStructure()
+        {
+            top = null;
+        }
+        public void Push(int data) /* Ekleme */
+        {
+            Node eleman = new Node(data);
+            if(top == null)
+            {
+                top = eleman;
+                Console.WriteLine("Stack yapısı oluşturuldu.İlk eleman eklendi");
+            }
+            else
+            {
+
+            }
+        } 
+    }
+}
