6aa048f60da524c67120ecf6f076f73e9d8e2d5c
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-03-08 20:42:37 +0300

    HashTable data structure has been entered.

--- DataStructures.HashTable/Node.cs
+++ DataStructures.HashTable/Node.cs
@@ -8,6 +8,18 @@ namespace DataStructures.HashTable
 {
     public class Node
     {
-
+        public int key;
+        public string isim;
+        public Node next;
+        public Node() //Dizinin içerisindeki gösterici olan düğüm.
+        {
+            this.next = null;
+        }
+        public Node(int key, string isim) //Dizinin dışında bağlı liste olarak duracak düğüm. Zinciri yapacağımız düğümler yani.
+        {
+            this.key = key;
+            this.isim = isim;
+            this.next = null;
+        }
     }
 }
---
+++ DataStructures.HashTable/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.HashTable
+{
+    public class Table
+    {
+        int size;
+        Node[] dizi; 
+        public Table(int size) // Oluşturulacak dizinin boyutu.
+        {
+            this.size = size;
+            dizi = new Node[size];  
+
+            for(int i = 0; i < size; i++) //Diziyi oluşturup içine düğümlerimizi yerleştirdik.Yani 3 isimli görseldeki işlemi gerçekleştirdik !
+            {
+                dizi[i] = new Node();
+            }
+        }
+    }
+}
