17d67041bcd8298c01dab8946fb8e837f12832a5
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-03-11 23:52:57 +0300

    Print function added and completed.

--- DataStructures.HashTable/Program.cs
+++ DataStructures.HashTable/Program.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace DataStructures.HashTable
 {
-    internal class Program
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            Table hTable = new Table(5);
+
+            hTable.Add(0, "Ahmet");
+            hTable.Add(10, "Mehmet");
+            hTable.Add(15, "Hasan");
+            hTable.Add(3, "Hüseyin");
+
+            /* 4.Görselde neyin nereye eklendiğini belirtmeye çalıştım. Mod'u 0 olanlar aynı dizi indexine zincir şeklinde ekleniyor. 3 ise index numarası 3 olan indexe ekleniyor.! Yani Hüseyin modu 3 olduğu için index numarası 3 olan kısma ekleniyor ! */
+        }
     }
 }
--- DataStructures.HashTable/Table.cs
+++ DataStructures.HashTable/Table.cs
@@ -9,15 +9,58 @@ namespace DataStructures.HashTable
     public class Table
     {
         int size;
-        Node[] dizi; 
+        Node[] dizi;
         public Table(int size) // Oluşturulacak dizinin boyutu.
         {
             this.size = size;
-            dizi = new Node[size];  
+            dizi = new Node[size];
 
-            for(int i = 0; i < size; i++) //Diziyi oluşturup içine düğümlerimizi yerleştirdik.Yani 3 isimli görseldeki işlemi gerçekleştirdik !
+            for (int i = 0; i < size; i++) //Diziyi oluşturup içine düğümlerimizi yerleştirdik.Yani 3 isimli görseldeki işlemi gerçekleştirdik !
             {
                 dizi[i] = new Node();
+            }
+        }
+        public int CreateIndex(int key) // Modunu alıyoruz düğümü ekleyeceğimiz indexi belirliyoruz.
+        {
+            return key % size;
+        }
+        public void Add(int key, string isim)
+        {
+            int index = CreateIndex(key);
+
+            Node eleman = new Node(key, isim);
+
+            Node temp = dizi[index];
+
+            if (temp.next == null)
+            {
+                temp.next = eleman;
+                Console.WriteLine("Sütunun ilk elemanı eklendi");
+            }
+            else
+            {
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = eleman;
+                Console.WriteLine(isim + " " + "eklendi");
+            }
+        }
+        public void Print()
+        {
+            /* Sütun sütun dizinin elemanlarını inceleyeceğiz. 0. index'den son index'e kadar eleman var mı kontrol edilecek. Eğer varsa önce o düğümlerin üzerinden geçilecek. Örneğin 0. index'de 3 düğüm varsa bu 3 düğüm gezildikten sonra diğer indexe gecilecek. */
+            for (int i = 0; i < size; i++)
+            {
+                Node temp = dizi[i]; //Bu whileden her cıktıgında bir artacak böylelikle tüm diziyi ve dizinin içerisindeki düğümleri gezmiş olacağız.
+                Console.WriteLine("dizi [{0}] => ", i);
+
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                    Console.Write(temp.key + " " + temp.isim);
+                }
+                Console.WriteLine();
             }
         }
     }
